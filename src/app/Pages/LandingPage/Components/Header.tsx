f61d995e251aabc88c3c1d7864f4fa553aa4d5dd
Marc <marc.blattmann08@gmail.com>
2024-08-13 18:02:19 +0200

    .

--- src/app/Pages/LandingPage/Components/Header.tsx
+++ src/app/Pages/LandingPage/Components/Header.tsx
@@ -1,13 +1,17 @@
 import Image from 'next/image';
+import Software from '@/app/Imgs/Software.png';
+import React from 'react';
 
 
 export default function Header() {
+
+
     return (
-        <div className="h-screen">
-            <div className="h-5/6 w-1/2 flex flex-col mb-64 justify-center">
-                <div className="flex flex-wrap w-9/12 justify-center items-center gap-5">
+        <div className="h-screen flex">
+            <div className="h-5/6 w-5/12 flex flex-col mb-20 justify-center">
+                <div className="flex flex-wrap w-full justify-center items-center gap-5">
                     <div className="flex gap-5 text-5xl font-black leading-snug w-9/12 flex-wrap">
-                        Learn <div className="bg-indigo-700 w-fit rounded-lg pr-2 pl-2 text-white">Smarter</div> and <div className="bg-red-500 w-fit rounded-lg pr-2 pl-2 text-white">Faster</div> with <div className="">EduSync</div> 
+                        Learn <div className="bg-indigo-700 w-fit rounded-lg pr-2 pl-2 text-white ">Smarter</div> and <div className="bg-red-500 w-fit rounded-lg pr-2 pl-2 text-white">Faster</div> with <div className="">EduSync</div> 
                     </div>
                     <div className="w-9/12 text-xl font-medium">EduSync helps manage your school day with learning and planning tools.</div>
                     <div className="w-9/12">
@@ -18,10 +22,8 @@ export default function Header() {
                 
                 
             </div>
-            <div className="h-screen w-1/2 flex justify-center items-center flex-col mb-64">
-                <div className="rounded-2xl">
-                    <Image src={} alt="Logo"/>
-                </div>
+            <div className="h-5/6 w-6/12 flex justify-center items-center flex-col mb-64 ml-36">
+            <iframe className='rounded-xl' width="800" height="415" src="https://www.youtube.com/embed/3VZFpwlXKpg?si=RqbnbMpEDYAOt2xB" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
     );
